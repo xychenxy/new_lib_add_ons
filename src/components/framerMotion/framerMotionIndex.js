79fbed6ff1c504da1b446c73657b1a1775568bb8
xychenxy <mavischen916@gmail.com>
2021-07-01 23:56:50 +1000

    developer 3

--- src/components/framerMotion/framerMotionIndex.js
+++ src/components/framerMotion/framerMotionIndex.js
@@ -96,7 +96,7 @@ const FramerMotionIndex = () => {
 				stop
 			</button>
 			<p>test git 12345</p>
-			<p>feature001 1 2</p>
+			<p>feature001 1 2 3</p>
 			<div>
 				<motion.div className="box" animate={control}></motion.div>
 			</div>
